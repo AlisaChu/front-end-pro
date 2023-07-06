a2db69968b2cd8d1f0eecca02261bee3928e4c40
AlisaChu <shendrovska89@gmail.com>
2023-07-06 22:26:59 +0200

    pull branch

--- calc_2/index.js
+++ calc_2/index.js
@@ -1,4 +1,21 @@
 'use strict';
+
+const calculator = createCalculator(100);
+console.log(calculator.get()); // 40
+
+calculator.reset();
+console.log(calculator.get()); // 100
+
+calculator.add(10);
+calculator.add(10);
+calculator.sub(20);
+
+calculator.set(20);
+
+calculator.add(10);
+calculator.add(10);
+calculator.add('qwe'); // NaN and value 40 do not change
+
 function createCalculator(base) {
     let value = base;
     const initialValue = base;
@@ -32,19 +49,4 @@ function createCalculator(base) {
     };
 }
 
-const calculator = createCalculator(100);
 
-calculator.add(10);
-calculator.add(10);
-calculator.sub(20);
-
-calculator.set(20);
-
-calculator.add(10);
-calculator.add(10);
-calculator.add('qwe'); // NaN and value 40 do not change
-
-console.log(calculator.get()); // 40
-
-calculator.reset();
-console.log(calculator.get()); // 100
