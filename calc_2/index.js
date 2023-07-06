16cc9457753f1cc0257157a1f1fe02a4bc423e0d
AlisaChu <shendrovska89@gmail.com>
2023-07-06 22:39:00 +0200

    fix

--- calc_2/index.js
+++ calc_2/index.js
@@ -1,21 +1,4 @@
 'use strict';
-
-const calculator = createCalculator(100);
-console.log(calculator.get()); // 40
-
-calculator.reset();
-console.log(calculator.get()); // 100
-
-calculator.add(10);
-calculator.add(10);
-calculator.sub(20);
-
-calculator.set(20);
-
-calculator.add(10);
-calculator.add(10);
-calculator.add('qwe'); // NaN and value 40 do not change
-
 function createCalculator(base) {
     let value = base;
     const initialValue = base;
@@ -48,5 +31,23 @@ function createCalculator(base) {
         }
     };
 }
+const calculator = createCalculator(100);
+
+calculator.add(10);
+calculator.add(10);
+calculator.sub(20);
+
+calculator.set(20);
+
+calculator.add(10);
+calculator.add(10);
+calculator.add('qwe'); // NaN and value 40 do not change
+
+console.log(calculator.get()); // 40
+
+calculator.reset();
+console.log(calculator.get()); // 100
+
+
 
 
