1b984dac82387cccc4c3f1482828d8c588df6181
AlisaChu <shendrovska89@gmail.com>
2023-07-19 19:41:32 +0200

    input fix

---
+++ input/index.js
@@ -0,0 +1,27 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', function() {
+    let btn = document.getElementById('msgButton');
+    let input = document.getElementById('msgInput');
+    let todoList = document.getElementById('todoList');
+
+    btn.addEventListener('click', function() {
+        let text = input.value.trim();
+
+        if(text === "") {
+            alert("Input cannot be empty");
+            return;
+        }
+
+        let li = document.createElement('li');
+        li.innerText = text;
+        todoList.prepend(li);
+
+        input.value = "";
+    });
+
+    input.addEventListener('keydown', function(e) {
+        if(e.key === "Enter") {
+            e.preventDefault();
+        }
+    });
+});
