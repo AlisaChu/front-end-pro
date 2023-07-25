1a75320dd5a19149de89fc12f22538bcf4150b49
AlisaChu <shendrovska89@gmail.com>
2023-07-25 19:05:39 +0200

    done

--- input_01/index.js
+++ input_01/index.js
@@ -1,72 +1,35 @@
-const firstNameInput = document.getElementById('firstName');
-const lastNameInput = document.getElementById('lastName');
-const phoneNumberInput = document.getElementById('phoneNumber');
+import { Contact, ContactTable } from './contact.js';
+
 const contactForm = document.getElementById('contactForm');
-const contactTableElement = document.getElementById('contactTable');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const phoneNumber = document.getElementById('phoneNumber');
 
-class Contact {
-    constructor(firstName, lastName, phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-    }
-}
-class ContactTable {
-    constructor(tableElement) {
-        this.table = tableElement;
-    }
-
-    addContact(contact) {
-        const rowHTML = `
-            <tr>
-                <td>${contact.firstName}</td>
-                <td>${contact.lastName}</td>
-                <td>${contact.phoneNumber}</td>
-                <td><button class="deleteBtn">Delete</button></td>
-            </tr>
-        `;
-        this.table.insertAdjacentHTML('beforeend', rowHTML);
-    }
-
-    deleteContact(buttonElement) {
-        const row = buttonElement.closest('tr');
-        if (row) {
-            row.remove();
-        }
-    }
-}
-
-
-const contactTable = new ContactTable(contactTableElement);
-
+const contactTable = new ContactTable('contactTable');
 contactForm.addEventListener('submit', function(event) {
     event.preventDefault();
-
-    if (!isValidInput(firstNameInput.value, lastNameInput.value, phoneNumberInput.value)) {
+    if (!isValidInput(firstName.value, lastName.value, phoneNumber.value)) {
         alert('Error: Invalid inputs. Please check and try again.');
         return;
     }
-
-    const contact = new Contact(firstNameInput.value, lastNameInput.value, phoneNumberInput.value);
+    const contact = new Contact(firstName.value, lastName.value, phoneNumber.value);
     contactTable.addContact(contact);
-
-    clearInputs();
+    clearForm();
 });
-
-contactTableElement.addEventListener('click', function(event) {
+contactTable.table.addEventListener('click', function(event) {
     if (event.target.className === 'deleteBtn') {
         contactTable.deleteContact(event.target);
     }
 });
-
 function isValidInput(firstName, lastName, phoneNumber) {
     if (firstName === '' || lastName === '' || phoneNumber === '' || isNaN(phoneNumber)) {
         return false;
     }
     return true;
 }
-function clearInputs() {
-    firstNameInput.value = '';
-    lastNameInput.value = '';
-    phoneNumberInput.value = '';
+
+function clearForm() {
+    firstName.value = '';
+    lastName.value = '';
+    phoneNumber.value = '';
 }
