dc65414c727a7023bd403a601a67bddf9102d0b0
AlisaChu <shendrovska89@gmail.com>
2023-07-29 13:37:45 +0200

    list to do

--- input/index.js
+++ input/index.js
@@ -1,22 +1,61 @@
-    let btn = document.getElementById('msgButton');
-    let input = document.getElementById('msgInput');
-    let todoList = document.getElementById('todoList');
-    btn.addEventListener('click', function() {
-        let text = input.value.trim();
+let btn = document.getElementById('msgButton');
+let input = document.getElementById('msgInput');
+let todoList = document.getElementById('todoList');
 
-        if(text === "") {
-            alert("Input cannot be empty");
-            return;
-        }
+btn.addEventListener('click', addTodoItem);
+input.addEventListener('keydown', handleEnterKey);
+todoList.addEventListener('click', handleTodoListClick);
 
-        let li = document.createElement('li');
-        li.innerText = text;
-        todoList.prepend(li);
+function addTodoItem() {
+    let text = sanitizeInput(input.value.trim());
 
-        input.value = "";
-    });
+    if(text === "") {
+        alert("Input cannot be empty");
+        return;
+    }
 
-    input.addEventListener('keydown', function(e) {
+    let li = createTodoItem(text);
+    todoList.prepend(li);
+    input.value = "";
+}
 
-    });
+function handleEnterKey(e) {
+    if(e.key === 'Enter') {
+        addTodoItem();
+    }
+}
 
+function handleTodoListClick(e) {
+    if(e.target.className === 'deleteBtn') {
+        deleteTodoItem(e.target);
+    } else if(e.target.tagName === 'LI') {
+        toggleTodoItemColor(e.target);
+    }
+}
+
+function createTodoItem(text) {
+    let li = document.createElement('li');
+    li.innerText = text;
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.className = 'deleteBtn';
+
+    li.append(deleteBtn);
+
+    return li;
+}
+
+function deleteTodoItem(button) {
+    todoList.removeChild(button.parentElement);
+}
+
+function toggleTodoItemColor(li) {
+    li.classList.toggle('green');
+}
+
+function sanitizeInput(input) {
+    let tempDiv = document.createElement('div');
+    tempDiv.textContent = input;
+    return tempDiv.innerHTML;
+}
