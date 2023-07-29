9f08761fa07eaa8ad1b9d31173512b3aedcf1bbc
AlisaChu <shendrovska89@gmail.com>
2023-07-29 13:42:41 +0200

    done

--- input/index.js
+++ input/index.js
@@ -1,61 +1,21 @@
 let btn = document.getElementById('msgButton');
 let input = document.getElementById('msgInput');
 let todoList = document.getElementById('todoList');
-
-btn.addEventListener('click', addTodoItem);
-input.addEventListener('keydown', handleEnterKey);
-todoList.addEventListener('click', handleTodoListClick);
-
-function addTodoItem() {
-    let text = sanitizeInput(input.value.trim());
+btn.addEventListener('click', function() {
+    let text = input.value.trim();
 
     if(text === "") {
         alert("Input cannot be empty");
         return;
     }
 
-    let li = createTodoItem(text);
-    todoList.prepend(li);
-    input.value = "";
-}
-
-function handleEnterKey(e) {
-    if(e.key === 'Enter') {
-        addTodoItem();
-    }
-}
-
-function handleTodoListClick(e) {
-    if(e.target.className === 'deleteBtn') {
-        deleteTodoItem(e.target);
-    } else if(e.target.tagName === 'LI') {
-        toggleTodoItemColor(e.target);
-    }
-}
-
-function createTodoItem(text) {
     let li = document.createElement('li');
     li.innerText = text;
+    todoList.prepend(li);
 
-    let deleteBtn = document.createElement('button');
-    deleteBtn.innerText = 'Delete';
-    deleteBtn.className = 'deleteBtn';
+    input.value = "";
+});
 
-    li.append(deleteBtn);
+input.addEventListener('keydown', function(e) {
 
-    return li;
-}
-
-function deleteTodoItem(button) {
-    todoList.removeChild(button.parentElement);
-}
-
-function toggleTodoItemColor(li) {
-    li.classList.toggle('green');
-}
-
-function sanitizeInput(input) {
-    let tempDiv = document.createElement('div');
-    tempDiv.textContent = input;
-    return tempDiv.innerHTML;
-}
+});
